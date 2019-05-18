c24458fbb2b19d55ce5e717a0951d56118301865
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2019-05-18 13:26:24 -0400

    added storybook

---
+++ .storybook/config.js
@@ -0,0 +1,28 @@
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
+
+const path = require("path");
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../")
+  });
+
+  // Return the altered config
+  return config;
+};
