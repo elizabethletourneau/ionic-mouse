b19578f45f30aa8560b1f95dcd7fd7b278368b6b
Elizabeth Letourneau <elizabeth.letourneau@shopify.com>
2019-05-18 13:26:24 -0400

    added storybook

---
+++ stories/index.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import Button from '../src/components/Button';
+import Navigation from "../src/components/Navigation";
+
+import { Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+    hello
+    </Button>
+  ));
+
+  storiesOf("Navigation", module)
+    .add("Dark", () => (
+      <Navigation dark/>
+    ));
